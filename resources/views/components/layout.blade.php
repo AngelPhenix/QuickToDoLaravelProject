efb921e90b3034165269b0a11d7530fb625d44d9
AngelPhenix <breathstylebro@gmail.com>
2024-07-18 15:11:41 +0200

    Added layout component and register page

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Todo App</title>
+</head>
+<body class="bg-[#15141f] h-full text-white p-3">
+        {{ $slot }}
+</body>
+</html>
