424d1b5648f566946eef74c8ab61a07bbe90daa4
AngelPhenix <breathstylebro@gmail.com>
2024-07-20 01:51:43 +0200

    Added middleware to routes for security purposes

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,13 @@ Route::get('/', function() {
     return view('welcome');
 });
 
-Route::post('/post_task', [TaskController::class, 'store']);
-Route::delete('/delete_task/{task}', [TaskController::class, 'destroy']);
-Route::get('/taskboard', [TaskController::class, 'index']);
+Route::post('/post_task', [TaskController::class, 'store'])->middleware('auth');
+Route::delete('/delete_task/{task}', [TaskController::class, 'destroy'])->middleware('auth');
+Route::get('/taskboard', [TaskController::class, 'index'])->middleware('auth');
 
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'create']);
 
 Route::get('/login', [SessionController::class, 'index']);
 Route::post('/login', [SessionController::class, 'store']);
-Route::get('/logout', [SessionController::class, 'destroy']);
+Route::get('/logout', [SessionController::class, 'destroy'])->middleware('auth');
