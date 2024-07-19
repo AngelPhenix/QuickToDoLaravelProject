f3137c6be4f719773d05c87943b76a4638148919
AngelPhenix <breathstylebro@gmail.com>
2024-07-20 01:36:15 +0200

    Added new changes to CSS again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,19 +6,22 @@
 
 
     @auth
-    <form method="post" action="/post_task">
+
+    <h1 class="mb-8">Welcome {{Auth::user()->name}}</h1>
+
+    <form method="post" action="/post_task" class="flex gap-x-3">
         @csrf
 
-        <input class="text-black" type="text" id="name" name="name"/>
+        <input class="text-black py-2 px-3" type="text" id="name" name="name"/>
         <button class="bg-slate-600 py-1 px-3">Add Task</button>
     </form>
 
-    <div class="mt-10">
+    <div class="mt-10 max-w-64">
         <p class="bg-cyan-950 text-center px-3 py-2"> TASKS </p>
             <ul>
             @foreach ($tasks as $task)
-                <li>
-                    {{ $task-> name }}
+                <li class="my-2">
+                    - {{ $task-> name }}
                     <form class="inline" method="post" action="/delete_task/{{ $task->id }}">
                         @csrf
                         @method('DELETE')
@@ -27,7 +30,6 @@
                 </li>
             @endforeach
             </ul> 
-        
     </div>
     @endauth
 </x-layout>
