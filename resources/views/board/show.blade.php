6df342cec62f26e5feeadfb551e1471e6bdada1d
AngelPhenix <breathstylebro@gmail.com>
2024-07-26 01:09:27 +0200

    Added board show view with input to add tasks to it

--- resources/views/board/show.blade.php
+++ resources/views/board/show.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
-    <h1 class="text-2xl">{{ $board->name }}</h1>
+    <h1 class="text-4xl">{{ $board->name }}</h1>
+    <p class="text-sm pb-10">Admin rights to : {{$board->owner->username}}</p>
+
+    <form method="post" action="/post_task/{{$board->id}}" class="flex gap-x-3">
+        @csrf
+
+        <input class="text-black py-2 px-3" type="text" id="name" name="name"/>
+        <button class="bg-slate-600 py-1 px-3">Add Task</button>
+    </form>
 </x-layout>
