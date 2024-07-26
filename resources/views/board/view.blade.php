665af67599ee9bfe07307a326b4f09c84ae31416
AngelPhenix <breathstylebro@gmail.com>
2024-07-27 00:53:44 +0200

    EXTREMELY SMALL AND USELESS css change for board cards

--- resources/views/board/view.blade.php
+++ resources/views/board/view.blade.php
@@ -2,7 +2,7 @@
     <h1 class="text-4xl py-10">-- Your boards --</h1>
     <div class="grid lg:grid-cols-4 gap-5">
         @foreach ($boards as $board)
-            <a class="text-2xl flex justify-center items-center bg-slate-500 w-[350px] h-[350px] hover:bg-slate-600" href="/board/{{ $board->id }}">{{ $board->name }}</a>
+            <a class="text-2xl flex justify-center items-center px-5 bg-slate-500 w-[350px] h-[350px] hover:bg-slate-600" href="/board/{{ $board->id }}">{{ $board->name }}</a>
         @endforeach
     </div>
 </x-layout>
