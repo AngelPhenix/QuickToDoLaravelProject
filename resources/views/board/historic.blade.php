8fb239bbda05c3fa3102e4991285c26af75a9741
AngelPhenix <breathstylebro@gmail.com>
2024-07-31 02:11:14 +0200

    Changed CSS depending on task->action

--- resources/views/board/historic.blade.php
+++ resources/views/board/historic.blade.php
@@ -2,8 +2,10 @@
     <ul>
         @foreach ($historicData as $data)
         @if ($data->board_id == $board->id)
-            @if ($data->action == "completed")
+            @if ($data->action == "done")
             <li class="text-green-500"><b>{{$data->modified_by}}</b> has done <u>{{$data->task_name}}</u> the [{{ $data->modified_at }}]</b></li>
+            @elseif ($data->action == "undone")
+            <li class="text-blue-500"><b>{{$data->modified_by}}</b> has done <u>{{$data->task_name}}</u> the [{{ $data->modified_at }}]</b></li>
             @else
             <li class="text-red-500"><b>{{$data->modified_by}}</b> has deleted <u>{{$data->task_name}}</u> the [{{ $data->modified_at }}]</b></li>
             @endif
