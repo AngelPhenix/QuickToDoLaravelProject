d3b9b38dd514847d9a0138121d919d7bd5327caa
AngelPhenix <breathstylebro@gmail.com>
2024-08-02 01:54:00 +0200

    Dealt with new graphic design for the modalé

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -7,28 +7,33 @@
     <!-- Modal background -->
     <div x-show="open" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
         <!-- Modal content -->
-        <div @click.away="open = false" class="bg-slate-500 text-black p-6 rounded shadow-lg">
+        <div @click.away="open = false" class="bg-slate-500 text-black p-6 rounded shadow-lg max-w-md">
             <h2 class="text-lg font-semibold">Add Labels</h2>
             <!-- Your form or content here -->
-            <form id="form_add" method="POST" action="/add_label/{{$task->id}}">
+            <form id="form_add" method="POST" action="/add_label/{{$task->id}}" class="flex items-center gap-x-2">
                 @csrf
-                <input type="text" id="name" name="name" class="p-1 border rounded">
-                <input type="color" name="color" class="border rounded">
-
+                <input type="text" id="name" name="name" class="p-2 border border-gray-300 rounded-md h-10" placeholder="Label name">
+                <input type="color" name="color" class="border border-gray-300 rounded-md h-10 w-12">
             </form>
 
-            <h2 class="text-sm font-semibold mb-1 mt-4"><u>Labels not used :</u></h2>
+            <h2 class="text-sm font-semibold mb-1 mt-4"><u>Select to toggle :</u></h2>
             @foreach ($labels as $label)
-                @if (!$task->labels->contains($label))
-                    <form method="POST" action="/update_task/{{ $task->id }}/label/{{ $label->id }}" class="inline-block">
+                {{-- @if (!$task->labels->contains($label))
+                    <form method="POST" action="/update_task/{{ $task->id }}/label/{{ $label->id }}" class="mb-1">
                         @csrf
                         @method('PATCH')
-                        <button type="submit" class="bg-[{{$label->color}}]/75 text-white px-2 rounded ">{{ $label->name }}</button>
+                        <button type="submit" class="bg-[{{$label->color}}]/75 text-white px-2 py-1 rounded">{{ $label->name }}</button>
                     </form>
-                @endif
+                @endif --}}
+
+                <form method="POST" action="/update_task/{{ $task->id }}/label/{{ $label->id }}" class="mb-1 inline-block">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit" class="bg-[{{$label->color}}]/75 text-white px-2 py-1 rounded truncate">{{ $label->name }}</button>
+                </form>
             @endforeach
 
-            <div class="flex justify-start mt-4">
+            <div class="flex justify-end mt-4">
                 <button type="button" @click="open = false" class="bg-red-500 text-white px-4 py-2 rounded mr-2">Cancel</button>
                 <button type="submit" form="form_add" class="bg-green-500 text-white px-4 py-2 rounded">Add</button>
             </div>
