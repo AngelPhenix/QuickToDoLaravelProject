c5ec80ee328c4cb397f11eae3142024de83f44a2
AngelPhenix <breathstylebro@gmail.com>
2024-08-07 00:19:24 +0200

    Added sidebar for informations

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,20 @@
 </head>
 <body class="bg-[#232326] h-full text-white p-3">
     <x-nav :board='$board ?? null'/>
+
+    <!-- Sidebar -->
+    <div class="w-64 h-screen bg-blue-900 text-white p-4">
+        <h2 class="text-xl font-semibold mb-4">Sidebar</h2>
+        <ul>
+            <li class="mb-2"><a href="#" class="hover:underline">Link 1</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Link 2</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Link 3</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Link 4</a></li>
+        </ul>
+        <h2 class="text-xl font-semibold mt-4 mb-2">Information</h2>
+        <p class="text-sm">Some information here.</p>
+    </div>
+
     <div class="flex flex-col items-center justify-center">
         {{ $slot }}
     </div>
