1b73bfa4caae757f21b628e18ff190c2a8af25de
AngelPhenix <breathstylebro@gmail.com>
2024-08-08 00:40:10 +0200

    Fixed sidebar colors and layout again

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,21 +1,21 @@
 @props(['board'])
 
-<div class="w-64 h-screen bg-blue-900 text-white p-4">
+<div class="w-64 h-screen bg-[#424246] text-white p-4">
     @guest
-        <nav class="flex flex-col">
-            <a href="/">Home</a>
-            <a href="/register">Register</a>
-            <a href="/login">Login</a>
-        </nav>
+    <nav class="flex flex-col">
+        <a href="/">Home</a>
+        <a href="/register">Register</a>
+        <a href="/login">Login</a>
+    </nav>
     @endguest
+    
     @auth
         <div class="flex flex-col gap-y-2">
-            <a class="px-2 py-1 bg-slate-500 rounded hover:bg-slate-600" href="/board_create">Create board</a>
+            <a class="px-2 py-1 bg-[#232326] rounded hover:bg-[#17171a]" href="/board_create">Create board</a>
 
             @if (isset($board))
-                <a href="/historic/board/{{$board->id}}">Logs</a>
+            <a href="/historic/board/{{$board->id}}">Logs</a>
             @endif
-
             <a href="/boards">Boards</a>
             <a href="/logout">Logout</a>
         </div>
