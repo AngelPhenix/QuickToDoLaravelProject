e75b2b80a68b4375e629207fc5a159ebbe35d2b4
AngelPhenix <breathstylebro@gmail.com>
2024-08-08 00:23:42 +0200

    Moved sidebar to its component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,13 @@
+@props(['board'])
+
+<div class="w-64 h-screen bg-blue-900 text-white p-4">
+    <h2 class="text-xl font-semibold mb-4">Sidebar</h2>
+    <ul>
+        <li class="mb-2"><a href="#" class="hover:underline">Link 1</a></li>
+        <li class="mb-2"><a href="#" class="hover:underline">Link 2</a></li>
+        <li class="mb-2"><a href="#" class="hover:underline">Link 3</a></li>
+        <li class="mb-2"><a href="#" class="hover:underline">Link 4</a></li>
+    </ul>
+    <h2 class="text-xl font-semibold mt-4 mb-2">Information</h2>
+    <p class="text-sm">Some information here.</p>
+</div>
