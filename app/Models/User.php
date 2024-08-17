00ecf1c9cb701ae2b6275a058488ee03bb395d12
AngelPhenix <breathstylebro@gmail.com>
2024-08-18 00:46:30 +0200

    Added belongsToMany relationship between users

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Label::class);
     }
+
+    public function friends()
+    {
+        return $this->belongsToMany(User::class, 'friend_user', 'user_id', 'friend_id');
+    }
 }
