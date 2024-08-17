e44cdc2082da77c8dcc855b9cccde7640cab2919
AngelPhenix <breathstylebro@gmail.com>
2024-08-17 13:18:07 +0200

    Added new form to add a friend from their mail

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,5 +1,14 @@
 <x-layout :boardList='$boardList ?? null'>
     <div class="flex flex-col items-center">
         <h1>Page de profil</h1>
+
+        <div class="flex flex-col items-center">
+            <p>Friendlist</p>
+            <form method="post" action="/addfriend">
+                @csrf
+                <input type="text" name="email" id="email" />
+                <button>Add</button>
+            </form>
+        </div>
     </div>
 </x-layout>
